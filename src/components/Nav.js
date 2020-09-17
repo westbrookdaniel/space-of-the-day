88f9c5009ba2e602f3bdf6fef14dc3f42cda9cc2
westy12dan <westy12dan@gmail.com>
2020-09-17 13:22:01 +0800

    responsive nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,21 +5,23 @@ import { GrFormSearch } from 'react-icons/gr'
 const Nav = () => {
 	return (
 		<header className="absolute w-full z-50 bg-primary-500">
-			<nav className="flex items-center justify-between px-12 m-auto flex-wrap">
+			<nav className="grid xl:flex grid-cols-2 items-center justify-between px-12 m-auto flex-wrap pb-4 xl:pb-0">
 				<Link
 					to="/about"
-					className="block mt-1 text-black font-bold hover:text-black mr-4"
+					style={{ gridRow: '2' }}
+					className="text-center block mt-1 text-black font-bold hover:text-black xl:mr-4"
 				>
 					About
 				</Link>
-				<Link to="/">
-					<h1 className="text-8xl tracking-tight m-3 pb-1 leading-none font-display text-black">
+				<Link to="/" style={{ gridRow: '1', gridColumn: 'span 2' }}>
+					<h1 className="text-center text-8xl tracking-tight m-3 pb-1 leading-none font-display text-black">
 						SPACE OF THE DAY
 					</h1>
 				</Link>
 				<Link
 					to="/create"
-					className="block mt-1 text-black hover:text-black mr-4"
+					style={{ gridRow: '2' }}
+					className="flex justify-center mt-1 text-black hover:text-black xl:mr-4"
 				>
 					<GrFormSearch title="search" size="2em" />
 				</Link>
