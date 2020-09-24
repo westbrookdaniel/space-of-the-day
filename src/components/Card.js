b5a49407c91748223a1e8c330c46fac40b36e39d
westy12dan <westy12dan@gmail.com>
2020-09-24 13:05:44 +0800

    cleanup card

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,9 @@ export default function Card({ day }) {
 					{day.date}
 				</span>
 				<img
-					className={`h-64 ${loading ? 'opacity-0' : null} transition-opacity ease-in-out duration-200 w-full object-cover`}
+					className={`h-64 ${
+						loading ? 'opacity-0' : null
+					} transition-opacity ease-in-out duration-200 w-full object-cover`}
 					onLoad={() => {
 						setLoading(false)
 					}}
@@ -23,13 +25,12 @@ export default function Card({ day }) {
 			<div className="px-3 pt-2 pb-5 flex-grow">
 				<div className="font-bold text-xl mb-1 mt-4">{day.title}</div>
 				<div className="text-base opacity-50">{day.copyright}</div>
-				{/* <p className="text-base">{day.explanation.substring(0, 100).trim()}...</p> */}
 			</div>
 			<div className="px-3 pb-4">
-				<button className="inline-block bg-primary-500 hover:bg-primary-600 active:bg-primary-700 transition-colors duration-200 ease-in-out text-black px-3 py-2 text-sm font-semibold mr-2">
+				<button className="focus:outline-none inline-block bg-primary-500 hover:bg-primary-600 active:bg-primary-700 transition-colors duration-200 ease-in-out text-black px-3 py-2 text-sm font-semibold mr-2">
 					Download
 				</button>
-				<button className="inline-block bg-white hover:bg-gray-400 active:bg-gray-500 transition-colors duration-200 ease-in-out text-black px-3 py-2 text-sm font-semibold mr-2">
+				<button className="focus:outline-none inline-block bg-white hover:bg-gray-400 active:bg-gray-500 transition-colors duration-200 ease-in-out text-black px-3 py-2 text-sm font-semibold mr-2">
 					Details
 				</button>
 			</div>
