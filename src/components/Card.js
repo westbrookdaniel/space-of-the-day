870fd40761358c04f689c7a60ab90384c695977a
westy12dan <westy12dan@gmail.com>
2020-09-24 14:05:47 +0800

    download link

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react'
+import { saveAs } from 'file-saver'
 import { Link } from 'react-router-dom'
 
 export default function Card({ day }) {
 	const [loading, setLoading] = useState(true)
+	const [isDownloading, setIsDownloading] = useState(false)
+
+	const handleDownload = async () => {
+		setIsDownloading(true)
+		const data = await fetch(`https://cors-anywhere.herokuapp.com/${day.hdurl}`)
+		const blob = await data.blob()
+		saveAs(blob, `${day.title}.jpg`)
+		setIsDownloading(false)
+	}
 
 	return (
 		<div className="max-w-sm overflow-hidden shadow-md h-full flex py-4 flex-col">
@@ -26,9 +36,23 @@ export default function Card({ day }) {
 				<div className="font-bold text-xl mb-1 mt-4">{day.title}</div>
 				<div className="text-base opacity-50">{day.copyright}</div>
 			</div>
-			<div className="px-3 pb-4">
-				<button className="focus:outline-none inline-block bg-primary-500 hover:bg-primary-600 active:bg-primary-700 transition-colors duration-200 ease-in-out text-black px-3 py-2 text-sm font-semibold mr-2">
-					Download
+			<div className="px-3 flex pb-4">
+				<button
+					onClick={handleDownload}
+					className="w-32 focus:outline-none inline-block bg-primary-500 hover:bg-primary-600 active:bg-primary-700 transition-colors duration-200 ease-in-out text-black px-3 py-2 text-sm font-semibold mr-2"
+				>
+					{isDownloading ? (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							className="animate-spin w-5 m-auto"
+						>
+							<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+						</svg>
+					) : (
+						<p>Download</p>
+					)}
 				</button>
 				<button className="focus:outline-none inline-block bg-white hover:bg-gray-400 active:bg-gray-500 transition-colors duration-200 ease-in-out text-black px-3 py-2 text-sm font-semibold mr-2">
 					Details
