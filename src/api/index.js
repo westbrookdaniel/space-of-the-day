8aa946953cba96e671697953e7b74c8b6b22e276
westy12dan <westy12dan@gmail.com>
2020-09-24 14:32:13 +0800

    remove videos

--- src/api/index.js
+++ src/api/index.js
@@ -6,13 +6,19 @@ const date = new Date()
 export default {
 	getMonth: async (month = date.getMonth() + 1) => {
 		try {
+			const out = []
 			const data = await fetch(
 				`https://api.nasa.gov/planetary/apod?api_key=${key}&start_date=${date.getFullYear()}-${
 					month - 1
 				}-${date.getDate()}`
 			)
 			const json = await data.json()
-			return json.reverse()
+			json.forEach(item => {
+				if (item.hdurl) {
+					out.push(item)
+				}
+			});
+			return out.reverse()
 		} catch (error) {
 			console.log(error)
 			return null
